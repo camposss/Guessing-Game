329a9fb4bc2043abd514849e527b70bf73c1ca49
camposss <ccamposs23@gmail.com>
2017-12-16 09:35:45 -0800

    set the value equal to the random number. gotta work on additional conditionals

--- src/components/game.js
+++ src/components/game.js
@@ -4,16 +4,57 @@ class Game extends Component {
     constructor(props){
         super(props);
 
-        this.state = {};
+        this.state = {
+            randomNumber: this.getRandomNumber(),
+            guessedNumber: ''
+        };
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
-
+    getRandomNumber(){
+        const randomNumber= Math.floor(Math.random()*10+1);
+        return randomNumber;
+    }
+    handleInputChange(e){
+        console.log(e.target.value);
+        this.setState({
+            guessedNumber: e.target.value
+        })
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        const {randomNumber}= this.state;
+        const {guessedNumber}= this.state;
+        console.log('Guessed Number in handleSubmit', guessedNumber);
+        console.log('Random Number in handleSubmit', randomNumber);
+        if (parseInt(guessedNumber)===randomNumber){
+           console.log('hello');
+        }
+    }
     render(){
+        const {randomNumber}=this.state;
+        const {guessedNumber}=this.state;
+        // console.log('Guessed Number', guessedNumber);
+        console.log('Random Number', randomNumber);
         return (
-            <div>
-                <h1 className="text-center my-3">Guessing Game</h1>
+            <div className='container'>
+                <h1 className="text-center my-3">Guess a Number between 1-10</h1>
+                    <form onSubmit= {this.handleSubmit}>
+                        <div className='form-group row'>
+                            <div className='col-8 push-2 text-center'>
+                                <input onChange={(e)=>{this.handleInputChange(e)}} className ='form-control text-center' type="text"/>
+                                <button  type='button' className='btn btn-outline-success'>Submit</button>
+                                <button type='button' className='btn btn-outline-danger'>Reset</button>
+                            </div>
+                        </div>
+                    </form>
+                <div className='row'>
+                    <div className='col-8 push-2 text-center'>
+                        display messages here
+                    </div>
+                </div>
             </div>
         )
     }
 }
-
 export default Game;
