bec9133174c3fdd457c3f3e7b9cfb4808e1dc958
camposss <ccamposs23@gmail.com>
2017-12-16 11:24:15 -0800

    committing

--- src/components/game.js
+++ src/components/game.js
@@ -10,6 +10,7 @@ class Game extends Component {
         };
         this.handleInputChange=this.handleInputChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
+        this.reset=this.reset.bind(this);
     }
     getRandomNumber(){
         const randomNumber= Math.floor(Math.random()*10+1);
@@ -28,13 +29,25 @@ class Game extends Component {
         console.log('Guessed Number in handleSubmit', guessedNumber);
         console.log('Random Number in handleSubmit', randomNumber);
         if (parseInt(guessedNumber)===randomNumber){
-           console.log('hello');
+           console.log('Kid you Guessed it!');
+        }else if (parseInt(guessedNumber) < randomNumber){
+            console.log('Too Low!');
         }
+        else if (parseInt(guessedNumber) > randomNumber){
+            console.log('Too High!');
+        }
+    }
+    reset(){
+        console.log('start over');
+        this.setState = {
+            randomNumber: this.getRandomNumber(),
+            guessedNumber: ''
+        };
     }
     render(){
         const {randomNumber}=this.state;
         const {guessedNumber}=this.state;
-        // console.log('Guessed Number', guessedNumber);
+        console.log('Guessed Number', guessedNumber);
         console.log('Random Number', randomNumber);
         return (
             <div className='container'>
@@ -44,7 +57,7 @@ class Game extends Component {
                             <div className='col-8 push-2 text-center'>
                                 <input onChange={(e)=>{this.handleInputChange(e)}} className ='form-control text-center' type="text"/>
                                 <button  type='button' className='btn btn-outline-success'>Submit</button>
-                                <button type='button' className='btn btn-outline-danger'>Reset</button>
+                                <button onClick={this.reset} type='button' className='btn btn-outline-danger'>Reset</button>
                             </div>
                         </div>
                     </form>
