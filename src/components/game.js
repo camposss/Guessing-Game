510f580c3e43ba66c0ce5d863605d7b79e46e18c
camposss <ccamposs23@gmail.com>
2017-12-16 17:10:21 -0800

    Working on local storage setting and getting

--- src/components/game.js
+++ src/components/game.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class Game extends Component {
     constructor(props){
         super(props);
-
         this.state = {
             randomNumber: this.getRandomNumber(),
-            guessedNumber: ''
+            guessedNumber: '',
+            display: '',
+            guessCounter: 0,
+            previousGuessedNumbers: this.props.previousGuessedNumbers
         };
         this.handleInputChange=this.handleInputChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleSubmittedGuess=this.handleSubmittedGuess.bind(this);
         this.reset=this.reset.bind(this);
     }
     getRandomNumber(){
@@ -22,50 +24,80 @@ class Game extends Component {
             guessedNumber: e.target.value
         })
     }
-    handleSubmit(e){
+    handleSubmittedGuess(e){
+        const {randomNumber,guessedNumber}= this.state;
+        let {guessCounter}= this.state;
+        // localStorage.setItem('previousGuessedNumbers', JSON.stringify(previousGuessedNumbers));
         e.preventDefault();
-        const {randomNumber}= this.state;
-        const {guessedNumber}= this.state;
         console.log('Guessed Number in handleSubmit', guessedNumber);
         console.log('Random Number in handleSubmit', randomNumber);
+        if (guessedNumber===''){
+            this.setState({display: 'Please enter a number'});
+        }
         if (parseInt(guessedNumber)===randomNumber){
            console.log('Kid you Guessed it!');
+           this.setState({
+               display: 'Kid you Guessed it',
+               guessCounter: guessCounter+1
+           });
+           setTimeout(this.reset,1000)
         }else if (parseInt(guessedNumber) < randomNumber){
             console.log('Too Low!');
+            this.setState({
+                    guessedNumber:'',
+                    display: 'Too Low!',
+                    guessCounter: guessCounter+1
+            });
         }
         else if (parseInt(guessedNumber) > randomNumber){
             console.log('Too High!');
+            this.setState({
+                guessedNumber:'',
+                display: 'Too High!',
+                guessCounter: guessCounter+1
+            });
         }
     }
     reset(){
-        console.log('start over');
-        this.setState = {
+        console.log('Resetting the Game');
+        const newState= {
             randomNumber: this.getRandomNumber(),
-            guessedNumber: ''
+            guessedNumber: '',
+            display: '',
+            guessCounter: 0
         };
+        this.setState(newState);
     }
     render(){
-        const {randomNumber}=this.state;
-        const {guessedNumber}=this.state;
+        const {randomNumber,guessedNumber, display, guessCounter}=this.state;
         console.log('Guessed Number', guessedNumber);
         console.log('Random Number', randomNumber);
+        console.log('Number of guesses:', guessCounter);
         return (
             <div className='container'>
                 <h1 className="text-center my-3">Guess a Number between 1-10</h1>
-                    <form onSubmit= {this.handleSubmit}>
+                    <form onSubmit= {(e)=>{this.handleSubmittedGuess(e)}}>
                         <div className='form-group row'>
                             <div className='col-8 push-2 text-center'>
-                                <input onChange={(e)=>{this.handleInputChange(e)}} className ='form-control text-center' type="text"/>
+                                <input onChange={(e)=>{this.handleInputChange(e)}} value={guessedNumber}
+                                       className ='form-control text-center' type="number" autoFocus
+                                />
                                 <button  type='button' className='btn btn-outline-success'>Submit</button>
                                 <button onClick={this.reset} type='button' className='btn btn-outline-danger'>Reset</button>
                             </div>
                         </div>
                     </form>
-                <div className='row'>
+                <div className='display-div row'>
                     <div className='col-8 push-2 text-center'>
-                        display messages here
+                        {display}
                     </div>
                 </div>
+                <div className='guessed-numbers-display row'>
+                    <div className='col-8 push-2 text-center'>
+                        {/*{previousGuessedNumbers}*/}
+                    </div>
+                </div>
+
             </div>
         )
     }
