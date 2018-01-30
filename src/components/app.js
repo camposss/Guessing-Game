f5085982eb8244145af0ebf8eb096e1f098bf979
camposss <ccamposs23@gmail.com>
2018-01-29 16:17:24 -0800

    Working on the audio file. Fixed up the css to make it more responsive.

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Game from './game'
 
 const App = () => (
-    <div className="container">
+    <div>
         <Game/>
     </div>
 );
--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import Sound from 'react-sound';
 
 class Game extends Component {
     constructor(props){
@@ -11,12 +10,21 @@ class Game extends Component {
             guessCounter: 0,
             previousGuessedNumbers: [],
             hasWon: false,
-            range: '10'
+            range: '10',
+            hasMounted: false
         };
         this.handleInputChange=this.handleInputChange.bind(this);
         this.handleSubmittedGuess=this.handleSubmittedGuess.bind(this);
         this.reset=this.reset.bind(this);
         this.changeRange= this.changeRange.bind(this);
+        this.rainforestMusic= new Audio('./assets/rainforest.mp3');
+    }
+    componentDidMount(){
+        this.setState({
+            hasMounted: true
+        });
+        this.rainforestMusic.play();
+
     }
     getRandomNumber(){
         const randomNumber= Math.floor(Math.random()*10+1);
@@ -25,33 +33,30 @@ class Game extends Component {
     handleInputChange(e){
         const {hasWon}=this.state;
         if(hasWon===false){
-            console.log(e.target.value);
             this.setState({
                 guessedNumber: e.target.value
             })
         }
     }
     handleSubmittedGuess(e){
+        e.preventDefault();
         const {randomNumber,guessedNumber,hasWon,previousGuessedNumbers}= this.state;
         let {guessCounter}= this.state;
-        localStorage.setItem('previousGuesses', guessedNumber);
-        previousGuessedNumbers.push(guessedNumber);
-        if(guessCounter===10){
+        if(guessedNumber!==''){
+            localStorage.setItem('previousGuesses', guessedNumber);
+            previousGuessedNumbers.push(guessedNumber);
+        }
+        if(guessCounter===5){
             this.reset();
             return;
         }
         if(!hasWon){
-        e.preventDefault();
-        console.log('Guessed Number in handleSubmit', guessedNumber);
-        console.log('Random Number in handleSubmit', randomNumber);
-        // console.log('previous guessed numbers in handleSubmit' ,previousGuessedNumbers);
             if (guessedNumber===''){
                 this.setState({
-                    display: 'Please enter a number',
+                    display: 'Please enter a number!',
                 });
             }
             if (parseInt(guessedNumber)===randomNumber){
-                console.log('Kid you Guessed it!');
                 this.setState({
                     display: 'Kid you Guessed it',
                     guessCounter: guessCounter+1,
@@ -60,7 +65,6 @@ class Game extends Component {
                 });
                 setTimeout(this.reset,1000)
             }else if (parseInt(guessedNumber) < randomNumber){
-                console.log('Too Low!');
                 this.setState({
                     guessedNumber:'',
                     display: 'Too Low!',
@@ -69,7 +73,6 @@ class Game extends Component {
                 });
             }
             else if (parseInt(guessedNumber) > randomNumber){
-                console.log('Too High!');
                 this.setState({
                     guessedNumber:'',
                     display: 'Too High!',
@@ -78,7 +81,8 @@ class Game extends Component {
                 });
             }
         }
-
+        let previousGuessContainer= document.getElementById('previousGuessContainer');
+        previousGuessContainer.scrollIntoView();
     }
     changeRange(){
         const {range}= this.state;
@@ -89,14 +93,12 @@ class Game extends Component {
             return;
         }
         const newRandomNumber= Math.floor(Math.random()*newRange+1);
-        console.log(newRandomNumber);
         this.setState({
             randomNumber: newRandomNumber,
             range: newRange
         })
     }
     reset(){
-        console.log('Resetting the Game');
         const newState= {
             randomNumber: this.getRandomNumber(),
             guessedNumber: '',
@@ -105,16 +107,11 @@ class Game extends Component {
             previousGuessedNumbers: [],
             hasWon:false
         };
-        localStorage.clear();
+        // localStorage.clear();
         this.setState(newState);
     }
     render(){
         const {randomNumber,guessedNumber, display, guessCounter,previousGuessedNumbers,range}=this.state;
-        // let previousGuessedNumbers=localStorage.getItem('previousGuesses');
-        console.log('previous guessed numbers' ,previousGuessedNumbers);
-        console.log('Guessed Number', guessedNumber);
-        console.log('Random Number', randomNumber);
-        console.log('Number of guesses:', guessCounter);
         let listOfPreviousGuesses=previousGuessedNumbers.map((item,index) =>{
             return(
                 <li key ={index} className='justify-content-center list-group-item list-group-item-warning'> Previous Guess | {item} </li>
@@ -126,19 +123,22 @@ class Game extends Component {
         };
         return (
             <div className='container'>
+                {/*{!this.state.hasMounted? <h2>Loading</h2>: ''}*/}
                 <div className='jumbotron'>
                     <h1 className="text-center my-3 ">Guess a Number between 1- {range}</h1>
-                    <small style={smallStyle}>You got 10 chances to hit it...Good Luck</small>
+                    <small style={smallStyle}>You got 5 chances to hit it...Good Luck</small>
                     <hr/>
                     <form onSubmit= {(e)=>{this.handleSubmittedGuess(e)}}>
                         <div className='form-group row'>
-                            <div className='col-8 push-2 text-center'>
+                            <div className='col-md-12 col-xs-12 text-center'>
                                 <input onChange={(e)=>{this.handleInputChange(e)}} value={guessedNumber}
-                                       className ='form-control text-center' type="number" autoFocus/>
-                                <button type='button' className='btn btn-outline-success'>Submit</button>
-                                <button onClick={this.reset} type='button' className='btn btn-outline-danger'>Reset</button>
-                                <button onClick={this.changeRange} type='button' className='btn btn-outline-warning'>Randomize Range</button>
+                                className ='form-control text-center' type="number" autoFocus/>
                             </div>
+                        </div>
+                        <div className=" buttonContainer row">
+                            <button type='button' className='col-xs-12 col-lg-4 btn btn-outline-success'>Submit</button>
+                            <button onClick={this.reset} type='button' className='col-xs-12 col-lg-4 btn btn-outline-danger'>Reset</button>
+                            <button onClick={this.changeRange} type='button' className=' col-xs-12 col-lg-4 btn btn-outline-warning'>Randomize Range</button>
                         </div>
                     </form>
                 <div className='display-div row'>
@@ -146,8 +146,8 @@ class Game extends Component {
                         {display}
                     </div>
                 </div>
-                <div className='guessed-numbers-display row'>
-                    <div className='col-8 push-2 text-center'>
+                <div className='row'>
+                    <div id = 'previousGuessContainer' className='col-md-8 offset-md-2 col-xs-12 text-center'>
                         <ul className='list-group'>
                             <li className='justify-content-center list-group-item list-group-item-danger'> Number of Guesses | {guessCounter}</li>
                             {listOfPreviousGuesses}
